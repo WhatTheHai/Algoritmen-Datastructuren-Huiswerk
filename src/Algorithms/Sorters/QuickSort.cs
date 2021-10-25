f193f9df0f190dd546d850f7abcfc1df0e558b70
WhatTheHai <haiboy@hotmail.nl>
2021-10-25 19:17:06 +0200

    Quicksort implemented

--- src/Algorithms/Sorters/QuickSort.cs
+++ src/Algorithms/Sorters/QuickSort.cs
@@ -9,8 +9,49 @@ namespace AD
 
         public override void Sort(List<int> list)
         {
-            // Should be implemented in lecture 4!
-            throw new System.NotImplementedException();
+            Sort(list, 0, list.Count-1);
+        }
+
+        public static void Sort(List<int> list, int low, int high) {
+            if (low + CUTOFF > high) {
+                new InsertionSort().Sort(list);
+            }
+            else {
+                int middle = (low + high) / 2;
+                //Sort low, middle, high for pivot
+                if (list[middle].CompareTo(list[low]) < 0) 
+                    SwapReferences(list, low, middle);
+
+                if (list[high].CompareTo(list[low]) < 0) 
+                    SwapReferences(list, low, high);
+
+                if (list[high].CompareTo(list[middle]) < 0) 
+                    SwapReferences(list, middle, high);
+
+                SwapReferences(list, middle, high);
+                int pivot = list[high - 1];
+
+                //Start partitioning
+                int i, j;
+                for (i = low, j = high - 1;;) {
+                    //Find value on left side to swap with right
+                    while (list[++i].CompareTo(pivot) < 0) ;
+                    while (pivot.CompareTo(list[--j]) < 0) ;
+                    if (i >= j)
+                        break;
+                    SwapReferences(list, i, j);
+                }
+                //Put pivot back
+                SwapReferences(list, i, high - 1);
+
+                Sort(list, low, i - 1);
+                Sort(list, i + 1, high);
+            }
+        }
+
+        private static void SwapReferences(List<int> list, int i, int j) {
+            //Swap using tuples
+            (list[i], list[j]) = (list[j], list[i]);
         }
     }
 }
--- src/Algorithms/Sorters/ShellSort.cs
+++ src/Algorithms/Sorters/ShellSort.cs
@@ -7,14 +7,14 @@ namespace AD
     public partial class ShellSort : Sorter
     {
         public override void Sort(List<int> list) {
-            //Knuth Sequence, 
             int gap = 1;
             int key, j;
             while (gap < list.Count/3) {
-                gap = gap*3 + 1;
+                //Knuth Sequence, 
+                //gap = gap*3 + 1;
 
                 //Version with division by 2.2
-                //gap = (int)(list.Count / 2.2);
+                gap = (int)(list.Count / 2.2);
             }
 
             while (gap >= 1) {
@@ -27,10 +27,11 @@ namespace AD
                     }
                     list[j] = key;
                 }
+                //Knuth Sequence
+                //gap /= 3;
 
-                gap /= 3;
                 //Version with divison by 2.2
-                //gap = (gap == 2) ? gap = 1 : (int)(gap / 2.2);
+                gap = (gap == 2) ? gap = 1 : (int)(gap / 2.2);
             }
         }
     }
